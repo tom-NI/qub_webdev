7f497181ae710a7a06df5f6f97730f8b221375f4
tt7182 <64065992+tt7182@users.noreply.github.com>
2021-02-05 09:06:17 +0000

    generated external nav and footer files

---
+++ site_navbar.php
@@ -0,0 +1,41 @@
+<?php
+    echo
+    "<nav class='navbar is-fixed-top is-info' role='navigation'>
+        <div class='navbar-brand'>
+
+            <div class='my_image_maintain_aspect'>
+                <img id='my_site_logo'
+                    src='http://tkilpatrick01.lampt.eeecs.qub.ac.uk/a_assignment_code/images/my_master_logo_small.png'
+                    alt='Premier League Logo' class='image navbar-item mt-3 mx-4'>
+            </div>
+            <a role='button' id='nav_burger' class='navbar-burger' aria-label='menu' aria-expanded='false'>
+                <span aria-hidden='true'></span>
+                <span aria-hidden='true'></span>
+                <span aria-hidden='true'></span>
+            </a>
+        </div>
+        <div class='navbar-menu'>
+            <div class='navbar-start'>
+                <a class='navbar-item my_nav_menu m-2' href='index.html'>Home</a>
+                <a class='navbar-item my_nav_menu m-2' href='fixture_analysis.html'>Analyse Fixtures</a>
+                <a class='navbar-item my_nav_menu m-2' href='advanced_search.html'>Advanced Search</a>
+                <a class='navbar-item my_nav_menu m-2' href='add_new_result.html'>Add Results</a>
+            </div>
+            <div class='navbar-end navbar-item has-dropdown is-hoverable pr-6'>
+                <a href='' class='navbar-link'>
+                    <span class='material-icons p-3'>language</span>
+                    <span>Language</span>
+                </a>
+                <div class='navbar-dropdown'>
+                    <a class='navbar-item'>English</a>
+                    <a class='navbar-item'>Français</a>
+                    <a class='navbar-item'>Deutsche</a>
+                    <a class='navbar-item'>Espanol</a>
+                    <a class='navbar-item'>Nederlands</a>
+                    <a class='navbar-item'>Italiano</a>
+                </div>
+            </div>
+        </div>
+    </nav>"
+
+?>
