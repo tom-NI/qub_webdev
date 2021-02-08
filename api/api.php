5a213b9f9c59bbab1d53b94d58596681e40f6f6e
tt7182 <64065992+tt7182@users.noreply.github.com>
2021-02-08 17:22:13 +0000

    API half working

--- api/api.php
+++ api/api.php
@@ -15,64 +15,74 @@
         INNER JOIN epl_clubs ON epl_clubs.ClubID = epl_matches.HomeClubID
         ORDER BY MatchID ASC;";
 
-        $queryValue = $conn->query($masterSqlQuery);
-        if (!$queryValue) {
+        $mainQueryValue = $conn->query($masterSqlQuery);
+        if (!$mainQueryValue) {
             echo $conn->error;
             die();
         }
 
         $matchDataSet = array();
 
-        while($row = $queryValue->fetch_assoc()) {
-            $homeClubID = $row["HomeClubID"];
-            $awayClubID = $row["AwayClubID"];
+        while($row = $mainQueryValue->fetch_assoc()) {
+            // $homeClubID = $row["HomeClubID"];
+            // $awayClubID = $row["AwayClubID"];
 
-            $homeClubNameQuery = "SELECT epl_clubs.ClubName AS 'HomeTeam', epl_clubs.ClubLogoURL AS 'HomeLogoURL' FROM `epl_clubs` WHERE ClubID = {$homeClubID}";
-            $awayClubNameQuery = "SELECT epl_clubs.ClubName AS 'AwayTeam', epl_clubs.ClubLogoURL AS 'AwayLogoURL'FROM `epl_clubs` WHERE ClubID = {$awayClubID}";
+            // $homeClubNameQuery = "SELECT epl_clubs.ClubName AS 'HomeTeam', epl_clubs.ClubLogoURL AS 'HomeLogoURL' FROM `epl_clubs` WHERE ClubID = {$homeClubID}";
+            // $awayClubNameQuery = "SELECT epl_clubs.ClubName AS 'AwayTeam', epl_clubs.ClubLogoURL AS 'AwayLogoURL'FROM `epl_clubs` WHERE ClubID = {$awayClubID}";
 
-            $homeClubQuery = $conn->query($homeClubNameQuery);
-            if (!$homeClubQuery) {
-                echo $conn->error;
-                die();
-            }
+            // $homeClubQuery = $conn->query($homeClubNameQuery);
+            // if (!$homeClubQuery) {
+            //     echo $conn->error;
+            //     die();
+            // }
 
-            $awayQueryValue = $conn->query($awayClubNameQuery);
-            if (!$awayQueryValue) {
-                echo $conn->error;
-                die();
-            }
-            
-            $matchDataSet[] = $row["MatchID"];
-            $matchDataSet[] = $row["SeasonYears"];
-            $matchDataSet[] = $row["MatchDate"];
-            $matchDataSet[] = $row["RefereeName"];
+            // $awayQueryValue = $conn->query($awayClubNameQuery);
+            // if (!$awayQueryValue) {
+            //     echo $conn->error;
+            //     die();
+            // }
 
-            while ($row = $homeClubQuery->fetch_assoc()) {
-                $matchDataSet[] = $row["HomeTeam"];
-                $matchDataSet[] = $row["HomeLogoURL"];
-            }
+            // while ($row = $homeClubQuery->fetch_assoc()) {
+            //     $homeTeamName = $row["HomeTeam"];
+            //     $homeTeamURL = $row["HomeLogoURL"];
+            // }
 
-            while ($row = $awayQueryValue->fetch_assoc()) {
-                $matchDataSet[] = $row["AwayTeam"];
-                $matchDataSet[] = $row["AwayLogoURL"];
-            }
+            // while ($row = $awayQueryValue->fetch_assoc()) {
+            //     $awayTeamName = $row["AwayTeam"];
+            //     $awayTeamURL = $row["AwayLogoURL"];
+            // }
 
-            $matchDataSet[] = $row["HTTotalGoals"];
-            $matchDataSet[] = $row["HTHalfTimeGoals"];
-            $matchDataSet[] = $row["HTShots"];
-            $matchDataSet[] = $row["HTShotsOnTarget"];
-            $matchDataSet[] = $row["HTCorners"];
-            $matchDataSet[] = $row["HTFouls"];
-            $matchDataSet[] = $row["HTYellowCards"];
-            $matchDataSet[] = $row["HTRedCards"];
-            $matchDataSet[] = $row["ATTotalGoals"];
-            $matchDataSet[] = $row["ATHalfTimeGoals"];
-            $matchDataSet[] = $row["ATShots"];
-            $matchDataSet[] = $row["ATShotsOnTarget"];
-            $matchDataSet[] = $row["ATCorners"];
-            $matchDataSet[] = $row["ATFouls"];
-            $matchDataSet[] = $row["ATYellowCards"];
-            $matchDataSet[] = $row["ATRedCards"];
+            $homeTeamName = null;
+            $homeTeamURL = null;
+            $awayTeamName = null;
+            $awayTeamURL = null;
+
+            $match = array(
+                "Season Year" => $row["SeasonYears"],
+                "Match Date" => $row["MatchDate"],
+                "Referee Name" => $row["RefereeName"],
+                "Home Team" => $homeTeamName,
+                "Home Team Logo URL" => $homeTeamURL,
+                "Away Team" => $awayTeamName,
+                "Away Team Logo URL" => $awayTeamURL,
+                "Home Team Total Goals" => $row["HTTotalGoals"],
+                "Home Team Half Time Goals" => $row["HTHalfTimeGoals"],
+                "Home Team Shots" => $row["HTShots"],
+                "Home Team Shots On Target" => $row["HTShotsOnTarget"],
+                "Home Team Corners" => $row["HTCorners"],
+                "Home Team Fouls" => $row["HTFouls"],
+                "Home Team Yellow Cards" => $row["HTYellowCards"],
+                "Home Team Red Cards" => $row["HTRedCards"],
+                "Away Team Total Goals" => $row["ATTotalGoals"],
+                "Away Team Half Time Goals" => $row["ATHalfTimeGoals"],
+                "Away Team Shots" => $row["ATShots"],
+                "Away Team Shots On Target" => $row["ATShotsOnTarget"],
+                "Away Team Corners" => $row["ATCorners"],
+                "Away Team Fouls" => $row["ATFouls"],
+                "Away Team Yellow Cards" => $row["ATYellowCards"],
+                "Away Team Red Cards" => $row["ATRedCards"]
+            );
+            $matchDataSet[] = $match;
         }
         echo json_encode($matchDataSet);
     }
