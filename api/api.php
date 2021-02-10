3262ea209d6183aa3dbe36457779cd6c2832c360
tt7182 <64065992+tt7182@users.noreply.github.com>
2021-02-10 16:33:00 +0000

    API match_summary built

--- api/api.php
+++ api/api.php
@@ -1,33 +1,21 @@
 <?php
-    // insert data and if it fails, print error message
+    header('Content-Type: application/json');
+    // query database and if it fails, print error message
     function dbQueryAndCheck($sqlQuery) {
         include("../dbconn.php");
-        $insertedValue = $conn->query($sqlQuery);
-        if (!$insertedValue) {
+        $queriedValue = $conn->query($sqlQuery);
+        if (!$queriedValue) {
             echo $conn->error;
             die();
+        } else {
+            return $queriedValue;
         }
     }
 
-    function buildJSONresponse($sqlQuery) {
-        $queryData = dbQueryAndCheck($sqlQuery);
-        $fullDataSet = array();
-        while ($row = $queryData->fetch_assoc()) {
-            $match = array(
-                "key" => $row["columnname"],
-            );
-            $fullDataSet[] = $match;
-        }
-        echo json_encode($fullDataSet);
-    }
-    
-    if (!isset($_GET['fulldb'])) {
+    if (!isset($_GET['eplinfo'])) {
         echo "<p>no data provided</p>";
     } else {
-        
-        header('Content-Type: application/json');
-
-        if (isset($_GET['ref_query'])) {
+        if (isset($_GET['ref_list'])) {
             // all referees query
             $refereeNameQuery = "SELECT RefereeName FROM `epl_referees` ORDER BY RefereeName ASC;";
             $refereeQueryData = dbQueryAndCheck($refereeNameQuery);
@@ -35,11 +23,11 @@
 
             while ($row = $refereeQueryData->fetch_assoc()) {
                 $ref = array(
-                    "referee" => $row["RefereeName"],
+                    "refereename" => $row["RefereeName"],
                 );
                 $refereeDataSet[] = $ref;
             }
-            echo json_encode($fullDataSet);
+            echo json_encode($refereeDataSet);
         } 
         if (isset($_GET['current_season'])) {
             // most recent season query from the API
@@ -53,16 +41,81 @@
                 );
                 $currentSeasonDataSet[] = $season;
             }
-            echo json_encode($fullDataSet);
-        } 
-        if (isset($_GET['current_seasons_clubs'])) {
+            echo json_encode($currentSeasonDataSet);
+        }
+        if (isset($_GET['current_season_clubs'])) {
             // query all current clubs from current season
-            $currentSeasonID = "SELECT SeasonID FROM `epl_seasons` ORDER BY SeasonID DESC LIMIT 1";
-    
+
+            $currentSeasonIDquery = "SELECT SeasonID FROM `epl_seasons` ORDER BY SeasonID DESC LIMIT 1";
+            $currentSeasonIDData = dbQueryAndCheck($currentSeasonIDquery);
+            while ($row = $currentSeasonIDData->fetch_assoc()) {
+                $seasonID = $row["SeasonID"];
+            }
+            
             $clubNameQuery = "SELECT DISTINCT epl_clubs.ClubName FROM `epl_clubs` 
             INNER JOIN epl_matches ON epl_matches.HomeClubID = epl_clubs.ClubID
             INNER JOIN epl_seasons ON epl_matches.SeasonID = epl_seasons.SeasonID
-            WHERE epl_seasons.SeasonID = {$currentSeasonID} ORDER BY ClubName ASC;";    
+            WHERE epl_seasons.SeasonID = {$seasonID} ORDER BY ClubName ASC;";
+
+            $clubQueryData = dbQueryAndCheck($clubNameQuery);
+            $clubsDataSet = array();
+            while ($row = $clubQueryData->fetch_assoc()) {
+                $clubnames = array(
+                    "clubname" => $row["ClubName"],
+                );
+                $clubsDataSet[] = $clubnames;
+            }
+            echo json_encode($clubsDataSet);
+        }
+        if (isset($_GET['match_summary']) && isset($_GET['2020'])) {
+            // todo - needs more work 
+            $matchSummaryQuery = "SELECT epl_matches.MatchID, epl_matches.MatchDate,
+            epl_matches.HomeClubID, epl_home_team_stats.HTTotalGoals, epl_away_team_stats.ATTotalGoals, epl_matches.AwayClubID
+            FROM epl_matches
+            INNER JOIN epl_home_team_stats ON epl_matches.MatchID = epl_home_team_stats.MatchID 
+            INNER JOIN epl_away_team_stats ON epl_matches.MatchID = epl_away_team_stats.MatchID
+            INNER JOIN epl_clubs ON epl_clubs.ClubID = epl_matches.HomeClubID
+            WHERE SeasonID = 1021
+            ORDER BY MatchID ASC";
+
+            $matchSummaryData = dbQueryAndCheck($matchSummaryQuery);
+            $matchSummaryDataSet = array();
+            while ($row = $matchSummaryData->fetch_assoc()) {
+                $homeClubID = $row["HomeClubID"];
+                $awayClubID = $row["AwayClubID"];
+
+                $homeClubNameQuery = "SELECT epl_clubs.ClubName, epl_clubs.ClubLogoURL FROM `epl_clubs` WHERE ClubID = {$homeClubID}";
+                $awayClubNameQuery = "SELECT epl_clubs.ClubName, epl_clubs.ClubLogoURL FROM `epl_clubs` WHERE ClubID = {$awayClubID}";
+
+                $homeClubQuery = dbQueryAndCheck($homeClubNameQuery);
+                $awayQueryValue = dbQueryAndCheck($awayClubNameQuery);
+                $homeTeamName;
+                $homeTeamURL;
+                $awayTeamName;
+                $awayTeamURL;
+
+                while ($homeTeamRow = $homeClubQuery->fetch_assoc()) {
+                    $homeTeamName = $homeTeamRow["ClubName"];
+                    $homeTeamURL = $homeTeamRow["ClubLogoURL"];
+                }
+
+                while ($awayTeamRow = $awayQueryValue->fetch_assoc()) {
+                    $awayTeamName = $awayTeamRow["ClubName"];
+                    $awayTeamURL = $awayTeamRow["ClubLogoURL"];
+                }
+
+                $matches = array(
+                    "matchdate" => $row["MatchDate"],
+                    "hometeam" => $homeTeamName,
+                    "homescore" => $row["HTTotalGoals"],
+                    "awayscore" => $row["ATTotalGoals"],
+                    "awayteam" => $awayTeamName,
+                    "hometeamlogoURL" => $homeTeamURL,
+                    "awayteamlogoURL" => $awayTeamURL
+                );
+                $matchSummaryDataSet[] = $matches;
+            }
+            echo json_encode($matchSummaryDataSet);
         }
     }
 ?>
