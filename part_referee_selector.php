d19424145dbf736e5e63944cf9050297ed29b95f
tt7182 <64065992+tt7182@users.noreply.github.com>
2021-02-13 11:39:21 +0000

    new file

---
+++ part_referee_selector.php
@@ -0,0 +1,10 @@
+<?php
+    $refereeAPIpath = "http://tkilpatrick01.lampt.eeecs.qub.ac.uk/a_assignment_code/api/api.php?list=ref_list";
+    $refereeAPIdata = file_get_contents($refereeAPIpath);
+    $refereeList = json_decode($refereeAPIdata, true);
+    
+    echo "<option value='default'>Select Referee</option>";
+    foreach($refereeList as $singleRef) {
+        echo "<option value='{$singleRef['refereename']}'>{$singleRef['refereename']}</option>";
+    }
+?>
