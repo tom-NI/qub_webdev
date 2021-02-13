b3486e71d50a5c552c3bb2d39d23812f21802433
tt7182 <64065992+tt7182@users.noreply.github.com>
2021-02-13 13:24:55 +0000

    word change

--- page_fixture_analysis.php
+++ page_fixture_analysis.php
@@ -7,14 +7,14 @@
             <div class="container column box is-centered my_sticky_div py-4 mx-5">
                 <div class="columns is-mobile is-vcentered is-centered">
                     <div class="column">
-                        <h4 class="is-size-4 is-size-5-mobile has-text-right team_a_name_colour"><b>Choose Team A</b>
+                        <h4 class="is-size-4 is-size-5-mobile has-text-right team_a_name_colour"><b>Team A</b>
                         </h4>
                     </div>
                     <div class="column level is-narrow mt-5">
                         <h4 class="level-item">vs.</h4>
                     </div>
                     <div class="column">
-                        <h4 class="is-size-4 is-size-5-mobile has-text-left team_b_name_colour"><b>Choose Team B</b>
+                        <h4 class="is-size-4 is-size-5-mobile has-text-left team_b_name_colour"><b>Team B</b>
                         </h4>
                     </div>
                 </div>
