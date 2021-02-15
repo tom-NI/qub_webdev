7ceece1fc4fd7e35aca8746d06745632a6c4b191
tt7182 <64065992+tt7182@users.noreply.github.com>
2021-02-15 16:06:19 +0000

    added real_escape etc to sql query

--- dbconn.php
+++ dbconn.php
@@ -4,7 +4,7 @@
     $pw = "h2mdZxjGdQLzrf3d";
     $db = "tkilpatrick01";
 
-    $conn = new mysqli($host, $user, $pw, $db); 
+    $conn = new mysqli_real_escape_string($host, $user, $pw, $db);
     
     if ($conn -> connect_error) {
         echo "there has been an error ".$conn->connect_error; 
