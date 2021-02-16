9c43058a6257ff683d20fa2471899e9ae136411b
tt7182 <64065992+tt7182@users.noreply.github.com>
2021-02-16 17:58:14 +0000

    modified for new server

--- dbconn.php
+++ dbconn.php
@@ -1,10 +1,12 @@
 <?php
-    $host = "tkilpatrick01.lampt.eeecs.qub.ac.uk";
+    // $host = "tkilpatrick01.lampt.eeecs.qub.ac.uk";
+    // $host = "lamp-19.eeecs.qub.ac.uk/phpmyadmin";
+    $host = "lamp-19.eeecs.qub.ac.uk";
     $user = "tkilpatrick01";
     $pw = "h2mdZxjGdQLzrf3d";
     $db = "tkilpatrick01";
 
-    $conn = new mysqli_real_escape_string($host, $user, $pw, $db);
+    $conn = new mysqli($host, $user, $pw, $db);
     
     if ($conn -> connect_error) {
         echo "there has been an error ".$conn->connect_error; 
