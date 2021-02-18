15850a45dbd71a4c89400914fa768b0d7ea4c7b9
tt7182 <64065992+tt7182@users.noreply.github.com>
2021-02-18 15:13:04 +0000

    GET finalised for single match links

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-    include("allfunctions.php");
+    include_once("allfunctions.php");
 ?>
 
 <!DOCTYPE html>
@@ -73,11 +73,12 @@
 
             <!-- 5 most recent premier league match results -->
             <?php
-                $recentMatchesURL = "http://tkilpatrick01.lampt.eeecs.qub.ac.uk/a_assignment_code/api/api.php?match_summaries&count=7";
+                $recentMatchesURL = "http://tkilpatrick01.lampt.eeecs.qub.ac.uk/a_assignment_code/api/api.php?match_summaries&count=8";
                 $recentMatchesAPIData = file_get_contents($recentMatchesURL);
                 $recentMatchesList = json_decode($recentMatchesAPIData, true);
 
                 foreach ($recentMatchesList as $summary) {
+                    $matchID = $summary['id'];
                     $matchDate = $summary['matchdate'];
                     $homeTeam = $summary['hometeam'];
                     $awayTeam = $summary['awayteam'];
@@ -89,49 +90,48 @@
                     // make the date decent looking!
                     $finalMatchDate = parseDateLongFormat($matchDate);
 
-                        echo "
-                        <a type='form' method='GET' href='page_single_match_result.php'>
-                        <div id='master_result_card' class='container box column is-centered my_box_border m-2 mb-5 mt-5 p-1'>
-                            <div>
-                                <p class='is-size-6 mt-3 is-size-7-mobile'>{$finalMatchDate}</p>
-                            </div>
-                            <div class='columns is-mobile is-vcentered is-centered'>
-                                <div class='column is-2 is-hidden-mobile is-narrow'>
-                                    <div class='is-pulled-right'>
+                    echo "
+                        <a href='page_single_match_result.php?id={$matchID}'>
+                            <div id='master_result_card' class='container box column is-centered my_box_border m-2 mb-5 mt-5 p-1'>
+                                <div>
+                                    <p class='is-size-6 mt-3 is-size-7-mobile'>{$finalMatchDate}</p>
+                                </div>
+                                <div class='columns is-mobile is-vcentered is-centered'>
+                                    <div class='column is-2 is-hidden-mobile is-narrow'>
+                                        <div class='is-pulled-right'>
+                                            <img class='image is-48x48 m-1 mb-5 my_image_maintain_aspect' src='{$homeLogo}' alt='Home Logo'>
+                                        </div>
+                                    </div>
+                                    <div class='column'>
+                                        <h4 class='is-size-6 is-size-6-mobile has-text-right is-narrow'>
+                                            <b>{$homeTeam}</b>
+                                        </h4>
+                                    </div>
+                                    <div class='column level mt-4 is-narrow'>
+                                        <div class='my_inline_divs result_box level-left'>
+                                            <p class='column is-size-5 is-size-6-mobile level-item p-1'>{$homeScore}</p>
+                                        </div>
+                                        <div class='my_inline_divs level-centre'>
+                                            <h4 class='level-item mx-2 is-size-7-mobile'>vs.</h4>
+                                        </div>
+                                        <div class='my_inline_divs result_box level-right'>
+                                            <p class='is-size-5 is-size-6-mobile column level-item p-1'>{$awayScore}</p>
+                                        </div>
+                                    </div>
+                                    <div class='column'>
+                                        <h4 class='is-size-6 is-size-6-mobile has-text-left is-narrow'>
+                                            <b>{$awayTeam}</b>
+                                        </h4>
+                                    </div>
+                                    <div class='column is-2 is-hidden-mobile is-narrow'>
                                         <img class='image is-48x48 m-1 mb-5 my_image_maintain_aspect'
-                                            src='{$homeLogo}'
-                                            alt='Home Logo'>
+                                            src='{$awayLogo}'
+                                            alt='Away Logo'>
                                     </div>
-                                </div>
-                                <div class='column'>
-                                    <h4 class='is-size-6 is-size-6-mobile has-text-right is-narrow'>
-                                        <b>{$homeTeam}</b>
-                                    </h4>
-                                </div>
-                                <div class='column level mt-4 is-narrow'>
-                                    <div class='my_inline_divs result_box level-left'>
-                                        <p class='column is-size-5 is-size-6-mobile level-item p-1'>{$homeScore}</p>
-                                    </div>
-                                    <div class='my_inline_divs level-centre'>
-                                        <h4 class='level-item mx-2 is-size-7-mobile'>vs.</h4>
-                                    </div>
-                                    <div class='my_inline_divs result_box level-right'>
-                                        <p class='is-size-5 is-size-6-mobile column level-item p-1'>{$awayScore}</p>
-                                    </div>
-                                </div>
-                                <div class='column'>
-                                    <h4 class='is-size-6 is-size-6-mobile has-text-left is-narrow'>
-                                        <b>{$awayTeam}</b>
-                                    </h4>
-                                </div>
-                                <div class='column is-2 is-hidden-mobile is-narrow'>
-                                    <img class='image is-48x48 m-1 mb-5 my_image_maintain_aspect'
-                                        src='{$awayLogo}'
-                                        alt='Away Logo'>
                                 </div>
                             </div>
-                        </div>
-                    </a>";
+                        </a>
+                    </form>";
                 }
             ?>
         </section>
@@ -324,11 +324,11 @@
                             </div>
                             <div class='tile is-12 is-mobile is-parent'>
                                 <div class='is-child box tile'>
-                                    <div class='my_inline_divs level my-2'>
+                                    <div class=' level my-2'>
                                         <div class='level-left level-item my_level_wrap'>
                                             <p class='has-text-left p-1'>{$lowTeamName}</p>
                                         </div>
-                                        <div class='level-right level-item'>
+                                        <div class='level-right level-item my-3'>
                                             <div>
                                                 <i class='material-icons redicon'>clear</i>
                                                 <p class='subtitle'><b>{$lowStat}</b></p>
@@ -340,11 +340,11 @@
                                     </div>
                                 </div>
                                 <div class='is-child box tile'>
-                                    <div class='my_inline_divs level my-2'>
+                                    <div class=' level my-2'>
                                         <div class='level-left level-item my_level_wrap'>
                                             <p class='has-text-left p-1'>{$highTeamName}</p>
                                         </div>
-                                        <div class='level-right level-item'>
+                                        <div class='level-right level-item my-3'>
                                             <div>
                                                 <i class='material-icons greenicon'>done</i>
                                                 <p class='subtitle'><b>{$highStat}</b></p>
