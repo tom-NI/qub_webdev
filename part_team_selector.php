2c8b6f2c1d403a3f2587f4a3d8b7fd159daa348f
tt7182 <64065992+tt7182@users.noreply.github.com>
2021-02-18 13:40:44 +0000

    partially complete for stickyness

--- part_team_selector.php
+++ part_team_selector.php
@@ -2,9 +2,34 @@
     $teamAPIpath = "http://tkilpatrick01.lampt.eeecs.qub.ac.uk/a_assignment_code/api/api.php?list=current_season_clubs";
     $teamAPIdata = file_get_contents($teamAPIpath);
     $teamList = json_decode($teamAPIdata, true);
+
+    if (isset($_GET['ht_selector'])) {
+        $HTSelected = $_GET['ht_selector'];
+    } else {
+        $HTSelected = null;
+    }
+    if (isset($_GET['at_selector'])) {
+        $ATSelected = $_GET['at_selector'];
+    } else {
+        $ATSelected = null;
+    }
     
     echo "<option value='default'>Select Team</option>";
     foreach($teamList as $singleTeam) {
-        echo "<option value='{$singleTeam['clubname']}'>{$singleTeam['clubname']}</option>";
+        if (isset($_GET['ht_selector'])) {
+            if ($HTSelected == $singleTeam['clubname']) {
+                echo "<option value='{$singleTeam['clubname']}' selected='selected'>{$singleTeam['clubname']}</option>";
+            } else {
+                echo "<option value='{$singleTeam['clubname']}'>{$singleTeam['clubname']}</option>";
+            } 
+        } elseif (isset($_GET['at_selector'])) { 
+            if($ATSelected == $singleTeam['clubname']) {
+                echo "<option value='{$singleTeam['clubname']}' selected='selected'>{$singleTeam['clubname']}</option>";
+            } else {
+                echo "<option value='{$singleTeam['clubname']}'>{$singleTeam['clubname']}</option>";
+            }
+        } else {
+            echo "<option value='{$singleTeam['clubname']}'>{$singleTeam['clubname']}</option>";
+        }
     }
 ?>
