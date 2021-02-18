772d630d3a19c25b2b6594e006ec8ef8ace48d78
tt7182 <64065992+tt7182@users.noreply.github.com>
2021-02-18 11:59:10 +0000

    made selection sticky

--- part_season_select.php
+++ part_season_select.php
@@ -2,8 +2,18 @@
     $seasonAPIpath = "http://tkilpatrick01.lampt.eeecs.qub.ac.uk/a_assignment_code/api/api.php?list=all_seasons_list";
     $seasonAPIdata = file_get_contents($seasonAPIpath);
     $seasonList = json_decode($seasonAPIdata, true);
+
+    if (isset($_GET['season_pref'])) {
+        $seasonSelected = $_GET['season_pref'];
+    } else {
+        $seasonSelected = null;
+    }
     
     foreach($seasonList as $singleSeason) {
-        echo "<option value='{$singleSeason['season']}'>{$singleSeason['season']}</option>";
+        if ($seasonSelected == $singleSeason['season']) {
+            echo "<option value='{$singleSeason['season']}' selected='selected'>{$singleSeason['season']}</option>";
+        } else {
+            echo "<option value='{$singleSeason['season']}'>{$singleSeason['season']}</option>";
+        }
     }
 ?>
