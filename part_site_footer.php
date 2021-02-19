46e65c07504671e7c7f193d394c60d3c1b8bbbe6
tt7182 <64065992+tt7182@users.noreply.github.com>
2021-02-19 12:53:55 +0000

    formatting

--- part_site_footer.php
+++ part_site_footer.php
@@ -1,59 +1,55 @@
-<?php
-    // site footer and social media buttons, common to all site pages
-    echo 
-        "<div class='level master_site_width mt-6'>
-        <div class='p-3'>
-            <div class='my_inline_divs mr-5 level-item'>
-                <p>Follow us on social media:</p>
-            </div>
-            <div class='my_inline_divs mr-5 level-item my_social_media_icon'>
-                <a href='https://www.facebook.com/' target='_blank'>
-                    <i class='fab fa-facebook fa-2x'></i>
-                </a>
-            </div>
-            <div class='my_inline_divs mr-5 level-item my_social_media_icon'>
-                <a href='https://www.youtube.com/' target='_blank'>
-                    <i class='fab fa-youtube fa-2x'></i>
-                </a>
-            </div>
-            <div class='my_inline_divs mr-5 level-item my_social_media_icon'>
-                <a href='https://twitter.com/' target='_blank'>
-                    <i class='fab fa-twitter fa-2x'></i>
-                </a>
-            </div>
-            <div class='my_inline_divs mr-5 level-item my_social_media_icon'>
-                <a href='https://www.instagram.com/' target='_blank'>
-                    <i class='fab fa-instagram fa-2x'></i>
-                </a>
-            </div>
+<div class='level master_site_width mt-6'>
+    <div class='p-3'>
+        <div class='my_inline_divs mr-5 level-item'>
+            <p>Follow us on social media:</p>
         </div>
-        <div class='my_inline_divs mr-5 '>
-            <button class='button is-rounded is-dark' id='back_to_top_button'>
-                <span class='icon is-left'>
-                    <i class='material-icons'>arrow_upward</i>
-                </span>
-                <span>Back to Top</span>
-            </button>
+        <div class='my_inline_divs mr-5 level-item my_social_media_icon'>
+            <a href='https://www.facebook.com/' target='_blank'>
+                <i class='fab fa-facebook fa-2x'></i>
+            </a>
+        </div>
+        <div class='my_inline_divs mr-5 level-item my_social_media_icon'>
+            <a href='https://www.youtube.com/' target='_blank'>
+                <i class='fab fa-youtube fa-2x'></i>
+            </a>
+        </div>
+        <div class='my_inline_divs mr-5 level-item my_social_media_icon'>
+            <a href='https://twitter.com/' target='_blank'>
+                <i class='fab fa-twitter fa-2x'></i>
+            </a>
+        </div>
+        <div class='my_inline_divs mr-5 level-item my_social_media_icon'>
+            <a href='https://www.instagram.com/' target='_blank'>
+                <i class='fab fa-instagram fa-2x'></i>
+            </a>
         </div>
     </div>
-    <footer class='footer pb-6'>
-        <div class='content master_site_width'>
-            <div class='has-text-centered'>
-                <p id='footer_text' class='mt-3'>© All Content Copyright EPL Match Statistic Finder</p>
-                <p><a href='>About us</a></p>
-                <p><a href='>Contact Us</a></p>
-                <p class='content m-4 subtitle is-7 mt-6'>The English Premier League Match Stat Finder website rejects
-                    racism in
-                    all its forms</p>
-                <p class='content m-4 subtitle is-7'>All statistics shown on this site are for the 2000-2001 league
-                    season onwards only.
-                    Earlier statistics are unavailable</p>
-                <p class='content m-4 subtitle is-7'>All information provided on the English Premier League Match
-                    Statistic Finder
-                    (EPLMSF) site is provided for entertainment and discussion purposes only and may not be used to make
-                    informed decisions about the Premier League. Past performances are not a reliable indicator of
-                    future results. Any performance metric inferences are incidental and
-                    are not designed to offend.</p>
-            </div>
-    </footer>";
-?>
+    <div class='my_inline_divs mr-5 '>
+        <button class='button is-rounded is-dark' id='back_to_top_button'>
+            <span class='icon is-left'>
+                <i class='material-icons'>arrow_upward</i>
+            </span>
+            <span>Back to Top</span>
+        </button>
+    </div>
+</div>
+<footer class='footer pb-6'>
+    <div class='content master_site_width'>
+        <div class='has-text-centered'>
+            <p id='footer_text' class='mt-3'>© All Content Copyright EPL Match Statistic Finder</p>
+            <p><a href='>About us</a></p>
+            <p><a href='>Contact Us</a></p>
+            <p class='content m-4 subtitle is-7 mt-6'>The English Premier League Match Stat Finder website rejects
+                racism in
+                all its forms</p>
+            <p class='content m-4 subtitle is-7'>All statistics shown on this site are for the 2000-2001 league
+                season onwards only.
+                Earlier statistics are unavailable</p>
+            <p class='content m-4 subtitle is-7'>All information provided on the English Premier League Match
+                Statistic Finder
+                (EPLMSF) site is provided for entertainment and discussion purposes only and may not be used to make
+                informed decisions about the Premier League. Past performances are not a reliable indicator of
+                future results. Any performance metric inferences are incidental and
+                are not designed to offend.</p>
+        </div>
+</footer>
