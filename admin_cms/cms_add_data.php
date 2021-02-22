dab7d0bfd10e549f872e77e279bfcff0ad891367
tt7182 <64065992+tt7182@users.noreply.github.com>
2021-02-22 14:37:59 +0000

    switched back to one page add data

--- admin_cms/cms_add_data.php
+++ admin_cms/cms_add_data.php
@@ -7,14 +7,14 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <script src="https://kit.fontawesome.com/06c5b011c2.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="../mystyles.css">
-    <title>Add a match result</title>
+    <link rel="stylesheet" href="mystyles.css">
+    <title>Upload match result</title>
 </head>
 
 <body class="has-navbar-fixed-top is-family-sans-serif">
     <!-- Full nav bar -->
     <?php
-        include("../part_site_navbar.php"); 
+        include("part_site_navbar.php"); 
     ?>
 
     <!-- banner at the top of the page! -->
@@ -30,13 +30,43 @@
 
     <div class="has-text-centered master_site_width container columns" id="my_upload_result_form">
         <div class="column is-8 is-offset-2">
+            <!-- add new club, ref, season data into db! -->
             <div class="mt-5 p-5 my_info_colour">
-                <div class='my_inline_divs my_info_colour mt-3'>
-                    <a href="cms_ref_club_seas.php"><button class="button is-rounded is-danger my_info_colour my-3">Add Referee, Club or Season</button></a>
+                <h2 class="title is-4 my_info_colour">Add new Clubs, Seasons or Referee Names</h2>
+                <h2 class="title is-5 mt-5 mb-1 my_info_colour">Add new Referee;</h2>
+                <div class="">
+                    <p class="p-2">Please enter Referee in the format "First initial. Surname" e.g. A. Referee</p>
+                    <form method="POST" action="logic_ref_club_season.php" class="level columns">
+                        <input type="text" required id="new_referee" name="newrefname" class="input level-item column is-5 mx-5 is-half-tablet" placeholder="Referee Name">
+                        <button class="button level-item is-danger m-3 is-rounded ">Add Referee</button>
+                    </form>
+                </div>
+                <h2 class="title is-5 mt-5 mb-1 my_info_colour">Add new Season;</h2>
+                <div class=""> 
+                    <p class="p-2">Please enter a season in the format "firstyear-secondyear" with 4 digits for each year e.g. 2000-2001</p>
+                    <?php
+                        require("allfunctions.php");
+                        $suggestedNextSeason = findNextSuggestedSeason();
+                        echo "
+                        <form method='POST' action='logic_ref_club_season.php' class='level columns'>
+                            <input type='text' required id='new_season' name='new_season' class='input level-item column is-5 mx-5 is-half-tablet' placeholder='Suggested next season to add : {$suggestedNextSeason}'>
+                            <button class='button level-item is-danger m-3 is-rounded my-3 '>Add New Season</button>
+                        </form>";
+                    ?>
+                </div>
+                <h2 class="title is-5 mt-5 mb-1 my_info_colour">Add new Club;</h2>
+                <div class="">
+                    <p class=" p-1">Please use the official club name and do not abbreviate.  Adding "football club" at the end is not required.</p>
+                    <p class="p-1">Club Logo URL must link directly to a .jpg or .png image file</p>
+                    <form method="POST" action="logic_ref_club_season.php" class="level columns">
+                        <input type="text" required id="new_club" name="new_club" class="input level-item column is-3 mx-2 is-one-third-tablet" maxlength="35" placeholder="Club Name (max 35 Characters)">
+                        <input type="url" required id="new_club_img_url" name="new_club_img_url" class="input level-item column is-3 mx-5 is-one-third-tablet" placeholder="Club Logo URL">
+                        <button class="button level-item is-danger is-rounded mt-4 my-3 ">Add New Club</button>
+                    </form>
                 </div>
             </div>
-            
-            <!-- add 1 new match details form  -->
+
+                    <!-- add 1 new match details form  -->
             <div class="field">
                 <form method="POST" action="logic_create_match_record.php">
                     <div class="mt-5 p-5 my_info_colour">
@@ -51,7 +81,7 @@
                                     <div class="select is-info my_small_form_item">
                                         <select class='my_small_form_item' name='select_season' id='select_season'>
                                             <?php
-                                                require("../part_season_select.php");
+                                                require("part_season_select.php");
                                             ?>
                                         </select>
                                     </div>
@@ -82,7 +112,7 @@
                                 <div class="select is-info my_small_form_item">
                                     <select class='my_small_form_item' name='select_ref' id='select_ref'>
                                         <?php
-                                            require("../part_referee_selector.php");
+                                            require("part_referee_selector.php");
                                         ?>
                                     </select>
                                 </div>
@@ -99,7 +129,7 @@
                             <div class="select is-success">
                                 <select class='my_small_form_item mx-2 ' name='ht_selector' id='ht_selector'>
                                     <?php
-                                        require("../part_team_selector.php");
+                                        require("part_team_selector.php");
                                     ?>
                                 </select>
                             </div>
@@ -111,7 +141,7 @@
                             <div class="select is-danger">
                                 <select class='my_small_form_item mx-2' name='at_selector' id='at_selector'>
                                     <?php
-                                        require("../part_team_selector.php");
+                                        require("part_team_selector.php");
                                     ?>
                                 </select>
                             </div>
@@ -206,7 +236,7 @@
         </div>
     </div>
 
-    <?php include("../part_site_footer.php"); ?>
+    <?php include("part_site_footer.php"); ?>
     <script src="my_script.js"></script>
 </body>
 
