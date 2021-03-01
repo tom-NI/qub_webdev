ba15f1854581c57581f5d03ef64bdfa337287fef
tt7182 <64065992+tt7182@users.noreply.github.com>
2021-03-01 15:16:04 +0000

    new all teams select

---
+++ part_pages/part_allteams_selector.php
@@ -0,0 +1,11 @@
+<?php
+    $teamAPIpath = "http://tkilpatrick01.lampt.eeecs.qub.ac.uk/epl_api_v1/list?all_clubs";
+    require("api_auth.php");
+    $teamAPIdata = file_get_contents($teamAPIpath, false, $context);
+    $teamList = json_decode($teamAPIdata, true);
+    
+    echo "<option value=''>Select Team</option>";
+    foreach($teamList as $singleTeamHome) {
+        echo "<option value='{$singleTeamHome['club']}'>{$singleTeamHome['club']}</option>";
+    }
+?>
