b6eaa3f4538d827719b1ec0a7f55bcd8871024ca
tt7182 <64065992+tt7182@users.noreply.github.com>
2021-03-11 15:17:52 +0000

    new emails directory

---
+++ email_templates.php/api_key_email.php
@@ -0,0 +1,32 @@
+<?php 
+    $emailBody = "
+    <!DOCTYPE html>
+    <html lang='en'>
+    <head>
+        <meta charset='UTF-8'>
+        <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+        <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+        <title>EPL Developer API Key</title>
+    </head>
+    <body>
+    <h3>Hi {$userFirstName}.</h3>
+    <h4>Thank you for your interest in the English Premier League API.</h4>
+
+    <p>You have set your Organization name as : </br>{$userOrganisation}</p>
+
+    <p>Your API key is :  </br>{$usersKey}</p>
+
+    <p>To authenticate all API requests, you must provide BOTH pieces of information in a basic Auth header. </br>
+    i.e. Your organisation name is a proxy for auth username, and your API key is a proxy for the auth password. </p>
+
+    <p>Please retain this email and keep your key secure - do not share the API key with anyone. </p>
+
+    <p>Kind Regards,</br>API team. </p>
+
+    <p>English Premier League, Match Statistic Finder Website.</br>
+    <p>The site for Premier League Match Statistics </p>
+
+    </body>
+    </html>";
+    
+?>
