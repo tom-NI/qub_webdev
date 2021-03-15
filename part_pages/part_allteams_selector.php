a357b4213b148d863cb7585dea27521f494f2f4e
tt7182 <64065992+tt7182@users.noreply.github.com>
2021-03-15 13:02:29 +0000

    debug

--- part_pages/part_allteams_selector.php
+++ part_pages/part_allteams_selector.php
@@ -1,5 +1,5 @@
 <?php
-    require("../logic_files/allfunctions.php");
+    include_once("logic_files/allfunctions.php");
     $teamAPIpath = "http://tkilpatrick01.lampt.eeecs.qub.ac.uk/epl_api_v1/clubs?all_clubs";
     $teamAPIdata = postDevKeyInHeader($teamAPIpath);
     $teamList = json_decode($teamAPIdata, true);
