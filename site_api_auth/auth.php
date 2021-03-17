1ebc53c6defcdc13b615ae5dc28ea3f3fb4a4423
tt7182 <64065992+tt7182@users.noreply.github.com>
2021-03-17 13:32:02 +0000

    new single auth file

---
+++ site_api_auth/auth.php
@@ -0,0 +1,8 @@
+<?php
+    // organisation name (proxy for username) 
+    // and developer key (proxy for password)
+    // for website authentication to USE API
+    // stored in one place to prevent redundancy issues
+    $orgName = "epl_main_site";
+    $keyValue = "492dd3-816c61-f89f93-e14f5f-e1566b";
+?>
