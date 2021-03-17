3e900624420234537d3cda9f7bdec038060de67f
tt7182 <64065992+tt7182@users.noreply.github.com>
2021-03-17 13:48:53 +0000

    setup current season

--- part_pages/part_current_season_team_selector.php
+++ part_pages/part_current_season_team_selector.php
@@ -1,33 +1,36 @@
 <?php
     $teamAPIpath = "http://tkilpatrick01.lampt.eeecs.qub.ac.uk/epl_api_v1/clubs?current_season_clubs";
-    include_once("logic_files/allfunctions.php");
+    include_once(__DIR__ . "/../logic_files/allfunctions.php");
     $teamAPIdata = postDevKeyInHeader($teamAPIpath);
     $teamList = json_decode($teamAPIdata, true);
 
-    if (isset($_GET['ht_selector']) && isset($_GET['at_selector'])) {
+    if (isset($_POST['ht_selector']) && isset($_POST['at_selector'])) {
         $HTSelected = $_GET['ht_selector'];
         $ATSelected = $_GET['at_selector'];
 
         // if on an edit page, dont show the default "select team" option
         if (!isset($homeTeamToEdit)) {
-            echo "<option value=''>Select Team</option>";
+            echo "<option value='Select Team'>Select Team</option>";
         }
         
         foreach($teamList as $singleTeam) {
             // for edit pages, set the selector to default to the existing team name
-            if (isset($homeTeamToEdit) && $homeTeamToEdit === $singleTeam['clubname']) {
-                echo "<option value='{$singleTeam['clubname']}' selected='selected'>{$singleTeam['clubname']}</option>";
-            }
-            if (isset($awayTeamToEdit) && $awayTeamToEdit === $singleTeam['clubname']) {
-                echo "<option value='{$singleTeam['clubname']}' selected='selected'>{$singleTeam['clubname']}</option>";
-            }
-
-            if ($HTSelected != null && $HTSelected === $singleTeam['clubname']) {
-                echo "<option value='{$singleTeam['clubname']}' selected='selected'>{$singleTeam['clubname']}</option>";
-            } elseif ($ATSelected != null && $ATSelected === $singleTeam['clubname']) {
-                echo "<option value='{$singleTeam['clubname']}' selected='selected'>{$singleTeam['clubname']}</option>";
+            if (isset($homeTeamToEdit) || isset($awayTeamToEdit)) {
+                if (isset($homeTeamToEdit) && $homeTeamToEdit === $singleTeam['clubname']) {
+                    echo "<option value='{$singleTeam['clubname']}' selected='selected'>{$singleTeam['clubname']}</option>";
+                } elseif (isset($awayTeamToEdit) && $awayTeamToEdit === $singleTeam['clubname']) {
+                    echo "<option value='{$singleTeam['clubname']}' selected='selected'>{$singleTeam['clubname']}</option>";
+                } else {
+                    echo "<option value='{$singleTeam['clubname']}' >{$singleTeam['clubname']}</option>";
+                }
             } else {
-                echo "<option value='{$singleTeam['clubname']}'>{$singleTeam['clubname']}</option>";
+                if ($HTSelected != null && $HTSelected === $singleTeam['clubname']) {
+                    echo "<option value='{$singleTeam['clubname']}' selected='selected'>{$singleTeam['clubname']}</option>";
+                } elseif ($ATSelected != null && $ATSelected === $singleTeam['clubname']) {
+                    echo "<option value='{$singleTeam['clubname']}' selected='selected'>{$singleTeam['clubname']}</option>";
+                } else {
+                    echo "<option value='{$singleTeam['clubname']}'>{$singleTeam['clubname']}</option>";
+                }
             }
         }
     } else {
