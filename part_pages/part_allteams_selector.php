3ee02b80fae9a25c4b8d80a0ec22fe79bff3e5b4
tt7182 <64065992+tt7182@users.noreply.github.com>
2021-03-17 21:04:38 +0000

    all teams <select> working for multi instances on one page

--- part_pages/part_allteams_selector.php
+++ part_pages/part_allteams_selector.php
@@ -3,9 +3,23 @@
     $teamAPIpath = "http://tkilpatrick01.lampt.eeecs.qub.ac.uk/epl_api_v1/clubs?all_clubs";
     $teamAPIdata = postDevKeyInHeader($teamAPIpath);
     $teamList = json_decode($teamAPIdata, true);
-    
+
     echo "<option value=''>Select Team</option>";
-    foreach($teamList as $singleTeamHome) {
-        echo "<option value='{$singleTeamHome['club']}'>{$singleTeamHome['club']}</option>";
+    if (!$htSelectorIsSet) {
+        foreach($teamList as $singleTeam) {
+            if (isset($_GET['ht_selector']) && $_GET['ht_selector'] == $singleTeam['club']) {
+                echo "<option value='{$singleTeam['club']}' selected='selected'>{$singleTeam['club']}</option>";
+            } else {
+                echo "<option value='{$singleTeam['club']}'>{$singleTeam['club']}</option>";
+            }
+        }
+    } else {
+        foreach($teamList as $singleTeam) {
+            if (isset($_GET['at_selector']) && $_GET['at_selector'] == $singleTeam['club']) {
+                echo "<option value='{$singleTeam['club']}' selected='selected'>{$singleTeam['club']}</option>";
+            } else {
+                echo "<option value='{$singleTeam['club']}'>{$singleTeam['club']}</option>";
+            }
+        }
     }
 ?>
