f44c61e5caea5a71b5c9f58729304d4fe12dfaf1
tt7182 <64065992+tt7182@users.noreply.github.com>
2021-03-24 17:55:37 +0000

    pagination and number bar part completed

--- part_pages/part_num_results_bar.php
+++ part_pages/part_num_results_bar.php
@@ -3,26 +3,37 @@
     <div class="field has-addons">
     <?php
             $currentPageURL = getCurrentPageURL();
-            $buttonValues = array(10, 25, 50);
+            $buttonValues = array(10, 20, 30);
+            
+            // remove any old queries from the current URL, before adding new. 
+            // means the URL cannot accumulate requests!
+            if (isset($_GET['totalresults'])) {
+                if (strpos($currentPageURL, "totalresults=") !== false) {
+                    str_replace("totalresults=", '', $currentPageURL);
+                }
+            }
 
             for ($i = 0; $i < 3; $i++) {
                 if (isset($_GET['totalresults'])) {
-                    $result = $_GET['totalresults'];
-                    if ($buttonValues[$i] === $result) {
-                        echo "
-                            <p class=control>
-                                <button action='$currentPageURL' name='totalresults' value='{$buttonValues[$i]}' method='GET' class='button is-small is-info is-outlined'>
-                                    <p>{$buttonValues[$i]}</p>
-                                </button>
-                            </p>";
-                    } else {
-                        echo "
-                            <p class=control>
-                                <button action='$currentPageURL' name='totalresults' value='{$buttonValues[$i]}' method='GET' class='button is-small is-info'>
-                                    <p>{$buttonValues[$i]}</p>
-                                </button>
-                            </p>";
-                    }
+                    $result = (int) htmlentities(trim($_GET['totalresults']));
+                } else {
+                    $result = 10;
+                }
+
+                if ($buttonValues[$i] == $result) {
+                    echo "
+                        <p class=control>
+                            <a type='button' href='{$currentPageURL}?totalresults={$buttonValues[$i]}'>
+                                <p class='button is-small is-info'>{$buttonValues[$i]}</p>
+                            </a>-
+                        </p>";
+                } else {
+                    echo "
+                        <p class=control>
+                            <a type='button' href='{$currentPageURL}?totalresults={$buttonValues[$i]}'>
+                                <p class='button is-small is-info is-outlined'>{$buttonValues[$i]}</p>
+                            </a>
+                        </p>";
                 }
             }
         ?>
--- part_pages/part_pagination.php
+++ part_pages/part_pagination.php
@@ -2,29 +2,53 @@
     <span class="mr-3">
         <p>Go to Page :</p>
     </span>
-    <a href="" class="pagination-previous is-outlined button is-info" disabled>Previous</a>
-    <a href="" class="pagination-next is-outlined button is-info">Next</a>
+    <a href="" id='previous_page_btn' class="pagination-previous is-outlined button is-info" disabled>Previous</a>
+    <a href="" id='next_page_btn' class="pagination-next is-outlined button is-info">Next</a>
     <ul class="pagination-list is-centered">
         <!-- total results / results per page - display total as a for loop -->
-        <?php
+        <?php           
             // only show page array if there are pages to show
+            // $totalMatchesToDisplay comes from part_print_summaries (calculates total items to display)
             if ($totalMatchesToDisplay > 0) {
                 if (isset($_GET['totalresults'])) {
                     $resultsPerPage = htmlentities(trim($_GET['totalresults']));
                     $totalPages = (int) ceil($totalMatchesToDisplay / $resultsPerPage);
+                    
+                    // limit total displayed pages to ten
+                    ($totalPages > 8) ? $totalPages = 8 : $totalPages;
+                    $numResultsReturnedQuery = "&count={$resultsPerPage}";
                 } else {
-                    $resultsPerPage = 10;
+                    $totalPages = 8;
+                    $numResultsReturnedQuery = "&count=10";
                 }
-                $totalPages = (int) ceil($totalMatchesToDisplay / $resultsPerPage);
-                for ($i = 0; $i < $totalPages; $i++) {
-                    if (isset($pageNum)) { // page number is something - set the button to be 
-                        $pageNum = $i + 1;
-                        echo "<li><a href='page_advanced_search.php?totalresults={$resultsPerPage}&pagenumber={$pageNum}&userfilter' 
-                        class='pagination-link is-info button is-outlined'>{$pageNum}</a></li>";
+
+                if (isset($_GET['pagenumber'])) {
+                    $pageNumber = (int) htmlentities(trim($_GET['pagenumber']));
+                } else {
+                    $pageNumber = 1;
+                }
+            
+                if ($pageNumber > 1) {
+                    if (isset($_GET['totalresults'])) {
+                        $resultCount = (int) htmlentities(trim($_GET['totalresults']));
+                    } else {
+                        $resultCount = 10;
+                    }
+                    $pageQuery = "&startat{$resultCount}";
+                } else {
+                    $pageQuery = "&startat=0";
+                }
+
+                $currentPageURL = getCurrentPageURL();
+                
+                // numResultsReturnedQuery and pageQuery of these variables calculated at the top of the pages with pagination for the API request
+                $newPageURL = "{$currentPageURL}{$numResultsReturnedQuery}{$pageQuery}";
+
+                for ($i = 1; $i <= $totalPages; $i++) {
+                    if (isset($pageNum)) {
+                        echo "<li><a href='{$newPageURL}' class='pagination-link is-info button is-outlined'>{$i}</a></li>";
                     } elseif (!isset($pageNum)) {
-                        $pageNum = $i + 1;
-                        echo "<li><a href='page_advanced_search.php?totalresults={$resultsPerPage}&pagenumber={$pageNum}&userfilter' 
-                        class='pagination-link is-info button'>{$pageNum}</a></li>";
+                        echo "<li><a href='{$newPageURL}' class='pagination-link is-info button'>{$i}</a></li>";
                     }
                 }
             }
