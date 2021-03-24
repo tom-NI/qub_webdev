563a5ae0ddac0cadb0af7b22ad95cc7ea5b8e434
tt7182 <64065992+tt7182@users.noreply.github.com>
2021-03-24 16:39:26 +0000

    modified to for loop

--- part_pages/part_num_results_bar.php
+++ part_pages/part_num_results_bar.php
@@ -1,20 +1,30 @@
 <div class="level-item level-right">
     <p class="pr-3">Number of results:</p>
     <div class="field has-addons">
-        <p class="control">
-            <button action="page_advanced_search.php?totalresults?userfilter" method='GET' value='10' class="button is-small is-info">
-                <p>10</p>
-            </button>
-        </p>
-        <p class="control">
-            <button action="page_advanced_search.php?totalresults?userfilter" method='GET' value='25' class="button is-outlined is-small is-info ">
-                <p>25</p>
-            </button>
-        </p>
-        <p class="control">
-            <button action="page_advanced_search.php?totalresults?userfilter" method='GET' value='50' class="button is-outlined is-small is-info">
-                <p>50</p>
-            </button>
-        </p>
+    <?php
+            $currentPageURL = getCurrentPageURL();
+            $buttonValues = array(10, 25, 50);
+
+            for ($i = 0; $i < 3; $i++) {
+                if (isset($_GET['totalresults'])) {
+                    $result = $_GET['totalresults'];
+                    if ($buttonValues[$i] === $result) {
+                        echo "
+                            <p class=control>
+                                <button action='$currentPageURL' name='totalresults' value='{$buttonValues[$i]}' method='GET' class='button is-small is-info is-outlined'>
+                                    <p>{$buttonValues[$i]}</p>
+                                </button>
+                            </p>";
+                    } else {
+                        echo "
+                            <p class=control>
+                                <button action='$currentPageURL' name='totalresults' value='{$buttonValues[$i]}' method='GET' class='button is-small is-info'>
+                                    <p>{$buttonValues[$i]}</p>
+                                </button>
+                            </p>";
+                    }
+                }
+            }
+        ?>
     </div>
 </div>
