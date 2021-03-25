787dd1278a1669671865c53e862cf3b7d175bcf8
tt7182 <64065992+tt7182@users.noreply.github.com>
2021-03-25 17:17:40 +0000

    results bar working!

--- part_pages/part_num_results_bar.php
+++ part_pages/part_num_results_bar.php
@@ -2,41 +2,33 @@
     <p class="pr-3">Number of results:</p>
     <div class="field has-addons">
     <?php
-            $currentPageURL = getCurrentPageURL();
-            $buttonValues = array(10, 20, 30);
-            
-            // remove any old pagination queries from the current URL, before adding new. 
-            // means the URL cannot accumulate requests!
-            if (isset($_GET['totalresults'])) {
-                $num = (int) htmlentities(trim($_GET['totalresults']));
-                if (strpos($currentPageURL, "?totalresults=") !== false) {
-                    $cleanedURL = str_replace("?totalresults=", '', $currentPageURL);
-                    $finalURL = str_replace($num, '', $cleanedURL);
-                }
+        $currentPageURL = getCurrentPageURL();
+        $buttonValues = array(10, 20, 30);
+        
+        // remove any old pagination queries from the current URL, before adding new. 
+        // means the URL cannot accumulate requests!
+
+        for ($i = 0; $i < 3; $i++) {
+            if (isset($_GET['count'])) {
+                $resultsPerPage = (int) htmlentities(trim($_GET['count']));
+                // then remove it from the URL for the next query!
             } else {
-                $finalURL = $currentPageURL;
+                $resultsPerPage = 10;
             }
 
-            for ($i = 0; $i < 3; $i++) {
-                if (isset($_GET['totalresults'])) {
-                    $resultsPerPage = (int) htmlentities(trim($_GET['totalresults']));
-                } else {
-                    $resultsPerPage = 10;
-                }
-
-                // display the button as outlined or not - control variable for loop below
-                if ($buttonValues[$i] === $resultsPerPage) {
-                    $buttonOutline = "";
-                } else {
-                    $buttonOutline = "is-outlined";
-                }
-                // finally print the buttons with dynamic generated links and numbers
-                echo "
-                    <p class='control'>
-                        <a href='{$finalURL}?totalresults={$buttonValues[$i]}'>
-                            <span type='button' class='button is-small {$buttonOutline} is-info'>{$buttonValues[$i]}</span>
-                        </a>
-                    </p>";
+            // display the button as outlined or not - control variable for loop below
+            if ($buttonValues[$i] === $resultsPerPage) {
+                $buttonOutline = "";
+            } else {
+                $buttonOutline = "is-outlined";
+            }
+            // finally print the buttons with dynamic generated links and numbers
+            echo "
+                <p class='control'>
+                    <a href='?count={$buttonValues[$i]}'>
+                        <span type='button' class='button is-small {$buttonOutline} is-info'>{$buttonValues[$i]}</span>
+                    </a>
+                </p>";
             }
         ?>
     </div>
